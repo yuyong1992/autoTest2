046a05a149e643e8ce05bfc4f3a9473e0458cc7f
yuyong <yong.yu@sinnet.cn>
2019-12-02 17:37:18 +0800

    优化脚本：更新脚本中元素地址；优化日志输出；

--- cases/test_login.py
+++ cases/test_login.py
@@ -17,10 +17,18 @@ file_path = "E:\Desktop\\testdata_login.xlsx"
 file_sheetname = "correct"
 correct_exl = excel_to_dic.ExcelToDic(file_path, file_sheetname)
 correct_data = correct_exl.excel_to_dic()
-# 获取错误的登录测试数据
+# 获取手机号错误的登录测试数据
 err_phone_sheetname = "err_phone"
 err_phone_exl = excel_to_dic.ExcelToDic(file_path, err_phone_sheetname)
 err_phone_data = err_phone_exl.excel_to_dic()
+# 获取密码错误的测试数据
+err_pwd_sheetname = "err_pwd"
+err_pwd_exl = excel_to_dic.ExcelToDic(file_path, err_pwd_sheetname)
+err_pwd_data = err_pwd_exl.excel_to_dic()
+# 获取后台报错的测试数据
+err_backend_sheetname = "err_backend"
+err_backend_exl = excel_to_dic.ExcelToDic(file_path, err_backend_sheetname)
+err_backend_data = err_backend_exl.excel_to_dic()
 # 获取链接点击的测试数据
 link_click_sheetname = "link_click"
 link_click_exl = excel_to_dic.ExcelToDic(file_path, link_click_sheetname)
@@ -49,21 +57,24 @@ class LoginSuccess(unittest.TestCase):
         """登录成功"""
         loginpage = LoginPage(self.driver)
         loginpage.confirm_login_page()
-        print("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
+        # print("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
         logger1.info("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
         loginpage.wait(2)
         loginpage.type_username(data["username"])
         loginpage.type_pwd(data["pwd"])
-        loginpage.click_but(but_name="登录")
+        loginpage.click_login()
         loginpage.wait(10)
         try:
             self.assertIn(loginpage.get_page_title(), data["assert"])
-            print('%s Login Test Pass!' % data["username"])
-            logger1.info('%s Login Test Pass!' % data["username"])
+            # print('%s Login Test Pass!' % data["username"])
+            logger1.info('断言成功，%s 登录成功!' % data["username"])
+            logger1.info('--------------------')
             # loginpage.driver.quit()
         except AssertionError as e:
-            logger1.error('%s 登录失败！ err: %s' % (data["username"], e))
-            print('断言失败! err: %s' % e)
+            logger1.error('断言失败，%s 登录失败！ err: %s' % (data["username"], e))
+            loginpage.get_windows_img()
+            logger1.info('--------------------')
+            # print('断言失败! err: %s' % e)
             raise AssertionError
 
 
@@ -80,26 +91,74 @@ class LoginErr(unittest.TestCase):
         cls.driver.quit()
 
     @ddt.data(*err_phone_data)
-    def test_2_login_err(self, data):
+    def test_2_login_err_phone(self, data):
         """手机号格式错误"""
 
         loginpage = LoginPage(self.driver)
         loginpage.confirm_login_page()
-        print("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
+        # print("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
         logger2.info("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
-        loginpage.wait(2)
         loginpage.type_username(data["username"])
         loginpage.type_pwd(data["pwd"])
-        loginpage.click_but()
-        loginpage.sleep(1.5)
-        err_msg = loginpage.get_err_msg()
+        loginpage.click_login()
+        err_msg = loginpage.get_err_phone()
         try:
             self.assertEqual(err_msg, data["assert"])
             logger2.info("断言成功，提示正确")
-            print("断言成功，提示正确")
+            logger2.info('--------------------')
+            # print("断言成功，提示正确")
         except AssertionError as e:
-            logger2.error('断言失败！The wrong err_msg!')
-            print('断言失败 ! err: %s' % e)
+            logger2.error('断言失败，提示错误！err: % s' % e)
+            loginpage.get_windows_img()
+            logger2.info('--------------------')
+            # print('断言失败 ! err: %s' % e)
+            raise AssertionError
+
+    @ddt.data(*err_pwd_data)
+    def test_3_login_err_pwd(self, data):
+        """密码错误"""
+
+        loginpage = LoginPage(self.driver)
+        loginpage.confirm_login_page()
+        # print("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
+        logger2.info("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
+        loginpage.type_username(data["username"])
+        loginpage.type_pwd(data["pwd"])
+        loginpage.click_login()
+        err_msg = loginpage.get_err_pwd()
+        try:
+            self.assertEqual(err_msg, data["assert"])
+            logger2.info("断言成功，提示正确")
+            # print("断言成功，提示正确")
+        except AssertionError as e:
+            logger2.error('断言失败，提示错误!err：% s' % e)
+            # print('断言失败 ! err: %s' % e)
+            loginpage.get_windows_img()
+            logger2.info('--------------------')
+            raise AssertionError
+
+    @ddt.data(*err_backend_data)
+    def test_4_login_err_backend(self, data):
+        """后台报错"""
+
+        loginpage = LoginPage(self.driver)
+        loginpage.confirm_login_page()
+        # print("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
+        logger2.info("测试数据： ' %s ' ' %s ' ' %s '" % (data["用例名"], data["username"], data["pwd"]))
+        loginpage.type_username(data["username"])
+        loginpage.type_pwd(data["pwd"])
+        loginpage.click_login()
+        err_msg = loginpage.get_err_backend()
+        try:
+            self.assertEqual(err_msg, data["assert"])
+            logger2.info("断言成功，提示正确")
+            # print("断言成功，提示正确")
+            logger2.info('--------------------')
+        except AssertionError as e:
+            logger2.error('断言失败，提示错误!err：% s' % e)
+            loginpage.get_windows_img()
+            # print('断言失败 ! err: %s' % e)
+            logger2.info('--------------------')
             raise AssertionError
 
 
@@ -114,56 +173,58 @@ class LinkClick(unittest.TestCase):
     def tearDownClass(cls):
         cls.driver.quit()
 
-    def test_3_display_pwd(self):
+    def test_5_display_pwd(self):
         """显示和隐藏密码"""
-        path_display_pwd = 'xpath=>//*[@id="app"]/div/div[3]/div/div[1]/form/div[2]/i'
-        path_attr = 'xpath=>//*[@id="app"]/div/div[3]/div/div[1]/form/div[2]/input'
+
         loginpage = LoginPage(self.driver)
+        loginpage.type_username('18811133446')
+        loginpage.type_pwd('1234qwer')
         # 第一次点击，显示密码：输入框的type属性变为text
-        loginpage.click_but("显示密码按钮", path_display_pwd)
-        attr = loginpage.get_attr("type", path_attr)
+        loginpage.click_display_pwd()
+        attr = loginpage.get_attr("type", loginpage.path_pwd)
         try:
             self.assertEqual("text", attr)
-            logger3.info("断言成功!密码输入框的属性是%s" % attr)
-            print("断言成功!密码输入框的属性是%s" % attr)
+            logger3.info("断言成功，密码输入框的属性是 %s" % attr)
+            # print("断言成功!密码输入框的属性是%s" % attr)
+            logger3.info('--------------------')
         except AssertionError as e:
-            logger3.error("断言失败！err：%s" % e)
-            print("断言失败！err：%s" % e)
+            logger3.error("断言失败，密码输入框的属性是 % s. err：%s" % (attr, e))
+            loginpage.get_windows_img()
+            # print("断言失败！err：%s" % e)
+            logger3.info('--------------------')
             raise AssertionError
 
         # 第二次点击，隐藏密码：输入框的type属性变为password
-        loginpage.click_but("隐藏密码按钮", path_display_pwd)
-        attr2 = loginpage.get_attr("type", path_attr)
+        loginpage.click_display_pwd()
+        attr2 = loginpage.get_attr("type", loginpage.path_pwd)
         try:
             self.assertEqual("password", attr2)
-            logger3.info("断言成功!密码输入框的属性是%s" % attr2)
-            print("断言成功!密码输入框的属性是%s" % attr2)
+            logger3.info("断言成功，密码输入框的属性是%s" % attr2)
+            # print("断言成功!密码输入框的属性是%s" % attr2)
+            logger3.info('--------------------')
         except AssertionError as e:
-            logger3.error("断言失败！err：%s" % e)
-            print("断言失败！err：%s" % e)
+            logger3.error("断言失败，密码输入框的属性是 % s. err：%s" % (attr2, e))
+            loginpage.get_windows_img()
+            # print("断言失败！err：%s" % e)
+            logger3.info('--------------------')
             raise AssertionError
 
     @ddt.data(*link_click_data)
-    def test_4_link_click(self, data):
+    def test_6_link_click(self, data):
         """点击登录页面上的链接"""
-        # link_name = "验证码登录"
-        # path_link_capt = 'xpath=>//*[@id="app"]/div/div[3]/div/div[1]/form/div[3]/a'
-        # path_next_page = 'xpath=>//*[@id="app"]/div/div[3]/div/div[1]/form/div[2]/p'
-        # text_assert = "验证码"
+
         loginpage = LoginPage(self.driver)
-        loginpage.click_but(data["link_name"], data["path_link"])
-        loginpage.wait(5)
+        loginpage.click_link(data["path_link"], data["link_name"])
         text_next_page = loginpage.find_element(data["path_next_page"]).text
         try:
             self.assertIn(data["text_assert"], text_next_page)
-            logger3.info("\' %s \' 链接跳转成功！" % data["link_name"])
-            print("\' %s \' 链接跳转成功！" % data["link_name"])
+            logger3.info("断言成功，\' %s \' 链接跳转成功！" % data["link_name"])
+            # print("\' %s \' 链接跳转成功！" % data["link_name"])
             loginpage.back_browser()
+            logger3.info('--------------------')
         except AssertionError as e:
-            logger3.error("\' %s \' 链接跳转失败！err：%s" % (data["link_name"], e))
-            print("\' %s \' 链接跳转失败！err：%s" % (data["link_name"], e))
+            logger3.error("断言失败，\' %s \' 链接跳转失败！err：%s" % (data["link_name"], e))
+            loginpage.get_windows_img()
+            # print("\' %s \' 链接跳转失败！err：%s" % (data["link_name"], e))
+            logger3.info('--------------------')
             raise AssertionError
-
-
-if __name__ == '__main__':
-    unittest.main()
--- pages/login_page.py
+++ pages/login_page.py
@@ -7,23 +7,57 @@ logger = Logger('LoginPage').getlog()
 
 
 class LoginPage(BasePage):
+    # 手机号输入框位置
     path_username = 'xpath=>//*[@id="app"]/div/div/div[2]/div[2]/form/div[1]/input'
+    # 密码输入框位置
     path_pwd = 'xpath=>//*[@id="app"]/div/div/div[2]/div[2]/form/div[2]/input'
-    path_err = 'xpath=>/html/body/div[3]/p'
-    path_but = 'xpath=>//*[@id="app"]/div/div/div[2]/div[2]/form/div[4]'
+    # 手机号报错信息位置
+    path_err_phone = 'xpath=>//*[@id="app"]/div/div/div[2]/div[2]/form/div[1]/div'
+    # 密码报错信息位置
+    path_err_pwd = 'xpath=>//*[@id="app"]/div/div/div[2]/div[2]/form/div[2]/div[2]'
+    # 后台报错信息位置
+    path_err_backend = 'xpath=>//*[@id="app"]/div/div/div[2]/div[2]/form/div[4]/p'
+    # 登录按钮位置
+    path_but_login = 'xpath=>//*[@id="app"]/div/div/div[2]/div[2]/form/div[4]'
+    # 展示/隐藏密码按钮位置
+    path_display_pwd = 'xpath=>//*[@id="app"]/div/div/div[2]/div[2]/form/div[2]/div[1]/i'
 
+    # 输入手机号
     def type_username(self, username):
+        self.clear(self.path_username)
         self.type(self.path_username, username, input_name='手机号')
 
+    # 输入密码
     def type_pwd(self, pwd):
+        self.clear(self.path_pwd)
         self.type(self.path_pwd, pwd, input_name='密码')
 
-    def click_but(self):
-        self.click(self.path_but, but_name='登录')
+    # 点击登录按钮
+    def click_login(self):
+        self.click(self.path_but_login, but_name='登录')
 
-    def get_err_msg(self):
-        err_msg = self.find_element(self.path_err)
-        return err_msg.text
+    # 点击展示/隐藏密码按钮
+    def click_display_pwd(self):
+        self.click(self.path_display_pwd, '展示/隐藏密码')
+
+    # 点击登录页面上的链接
+    def click_link(self, path_link, name_link):
+        self.click(path_link, name_link)
+
+    # 获取页面上手机号输入框的错误信息
+    def get_err_phone(self):
+        err_msg_phone = self.find_element(self.path_err_phone)
+        return err_msg_phone.text
+
+    # 获取页面上密码输入框的错误信息
+    def get_err_pwd(self):
+        err_msg_pwd = self.find_element(self.path_err_pwd)
+        return err_msg_pwd.text
+    # 获取后台报错时页面上展示的错误信息
+
+    def get_err_backend(self):
+        err_msg_backend = self.find_element(self.path_err_backend)
+        return err_msg_backend.text
 
     # 用登录页面上的文字确认是否进入登录页面
     def confirm_login_page(self):
