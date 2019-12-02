792e7cfd789a6f401c90827b8c80981351729f26
yuyong <yong.yu@sinnet.cn>
2019-12-02 17:35:11 +0800

    优化基础框架中的日志输入

--- framework/browser_engine.py
+++ framework/browser_engine.py
@@ -20,43 +20,52 @@ class BrowserEngine(object):
 
     def open_browser(self, driver, test_server):
         config = ConfigParser()
-        file_path = os.path.dirname(
-            os.path.abspath('.')) + '/autoTest2/config/config.ini'
+        file_path = os.path.dirname(os.path.abspath('.')) + '/autoTest2/config/config.ini'
         config.read(file_path)
         # logger.info(file_path)
 
         browser = config.get("browserType", "browserName")
-        logger.info("You had select %s browser." % browser)
+        logger.info("设置测试浏览器为：% s" % browser)
         url = config.get(test_server, "URL")
-        logger.info("The test server url is: %s" % url)
+        # logger.info("设置测试地址为: %s" % url)
 
         if browser == "Firefox":
             # 设置无头浏览器，运行脚本不显示浏览器界面，减少脚本运行时间
-            firefox_opt = webdriver.FirefoxOptions()
-            firefox_opt.add_argument("--headless")
-            driver = webdriver.Firefox(firefox_options=firefox_opt)
-            logger.info("Starting firefox browser.")
+            # firefox_opt = webdriver.FirefoxOptions()
+            # firefox_opt.add_argument("--headless")
+            # driver = webdriver.Firefox(firefox_options=firefox_opt)
+
+            # 启用浏览器界面
+            driver = webdriver.Firefox()
+            logger.info("Firefox浏览器启动")
         elif browser == "Chrome":
             # 设置无头浏览器，运行脚本不显示浏览器界面，减少脚本运行时间
-            chrome_opt = webdriver.ChromeOptions()
-            chrome_opt.add_argument("--headless")
-            driver = webdriver.Chrome(self.chrome_driver_path, chrome_options=chrome_opt)
-            logger.info("Starting Chrome browser.")
+            # chrome_opt = webdriver.ChromeOptions()
+            # chrome_opt.add_argument("--headless")
+            # driver = webdriver.Chrome(self.chrome_driver_path, chrome_options=chrome_opt)
+
+            # 启用浏览器界面
+            driver = webdriver.Chrome()
+            logger.info("Chrome浏览器启动")
         elif browser == "IE":
             # 设置无头浏览器，运行脚本不显示浏览器界面，减少脚本运行时间
-            ie_opt = webdriver.IeOptions()
-            ie_opt.add_argument("--headless")
-            driver = webdriver.Ie(self.ie_driver_path, ie_options=ie_opt)
-            logger.info("Starting IE browser.")
+            # ie_opt = webdriver.IeOptions()
+            # ie_opt.add_argument("--headless")
+            # driver = webdriver.Ie(self.ie_driver_path, ie_options=ie_opt)
+
+            # 启用浏览器界面
+            driver = webdriver.Ie()
+            logger.info("IE浏览器启动")
 
         driver.maximize_window()
-        driver.get(url)
-        logger.info("Open url: %s" % url)
-        logger.info("Maximize the current window.")
+        logger.info("最大化浏览器界面")
         driver.implicitly_wait(10)
-        logger.info("Set implicitly wait 10 seconds.")
+        logger.info("设置隐式等待，最大等待时间：10s")
+        driver.get(url)
+        logger.info("访问URL: %s" % url)
+        logger.info("----------浏览器环境准备完成----------")
         return driver
 
     def quit_browser(self):
-        logger.info("Now, Close and quit the browser.")
         self.driver.quit()
+        logger.info("退出浏览器！")
